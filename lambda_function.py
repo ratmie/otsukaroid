ac25f2816aa0d786e0d1a47624c06d69275f45c7
Jumpei Takegata <takegata.j@gmail.com>
2018-10-30 23:17:30 +0900

    add import

---
+++ lambda_function.py
@@ -0,0 +1,30 @@
+import os
+import sys
+from linebot import {
+    LineBotApi, webhookHandler
+}
+from linebot.modls import {
+    MessageEvent, TextMessage, TextSenMessage
+}
+from linebot.exeptions import {
+    LineBotApiError, InvalidSignatureError
+}
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.ERROR)
+
+channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+if channel_secret is None:
+    logger.error('Specify LINE_CHANNEL_SECRET as environment variable.')
+    sys.exit(1)
+if channel_access_token is None:
+    logger.error('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+    sys.exit(1)
+
+line_bot_api = LineBotApi(channel_access_token)
+handler = WebhookHandler(channel_secret)
+
+def lambda_handler(event, context):
+    
